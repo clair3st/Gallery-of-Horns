eb31011b63f06685a2a9ab79fd641be0cc0a4778
clair3st <clairejgatenby@gmail.com>
2024-10-08 17:12:51 +1100

    finishing class 02, adding favorite button

---
+++ src/components/heart.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function Favorite() {
+  const [active, setActive] = useState(false)
+  const [fav, setFav] = useState(0)
+
+  function handleClick() {
+    if(fav == 0){
+      setActive(!active);
+
+    }
+    setFav(fav + 1);
+  }
+
+  return (
+    <div >
+ 
+      <span className={"favorite " + (active ? 'active': '')} onClick={handleClick}>{fav}</span>
+    </div>
+  );
+}
+
+export default Favorite
